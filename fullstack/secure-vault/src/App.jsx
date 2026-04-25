0bf7aed3c7d14e7d2399c9ed928580fab04a6575
garymurasira <g.murasira@alustudent.com>
2026-04-25 02:12:22 +0200

    added sidebar design

--- fullstack/secure-vault/src/App.jsx
+++ fullstack/secure-vault/src/App.jsx
@@ -1,10 +1,14 @@
 import Topbar from './components/Topbar/Topbar'
+import Sidebar from './components/Sidebar/Sidebar'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
       <Topbar />
+      <div className="workspace">
+        <Sidebar />
+      </div>
     </div>
   )
 }
---
+++ fullstack/secure-vault/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__header">
+        <span className="sidebar__title">File Explorer</span>
+        <div className="search-bar">
+          <svg className="search-bar__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+          <input
+            className="search-bar__input"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+
+      <div className="sidebar__tree">
+        {/* Tree will go here */}
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
