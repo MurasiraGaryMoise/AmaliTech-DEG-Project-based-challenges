a79414f9da7847150831ea3be38c89f3e8b02f80
garymurasira <g.murasira@alustudent.com>
2026-04-27 01:57:18 +0200

    replaced mock vars with useState

--- fullstack/SupportFlow-Visual-Builder/src/App.jsx
+++ fullstack/SupportFlow-Visual-Builder/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Canvas from './components/Canvas/Canvas'
@@ -6,32 +7,73 @@ import PreviewChat from './components/PreviewChat/PreviewChat'
 import AddNodeModal from './components/AddNodeModal/AddNodeModal'
 import flowData from '../flow_data.json'
 
-const mockSelectedNode = flowData.nodes[1]
-const isPreviewMode = false
-const showAddNodeModal = true
-
 function App() {
+  const [nodes, setNodes] = useState(flowData.nodes)
+  const [isPreviewMode, setIsPreviewMode] = useState(false)
+  const [selectedNodeId, setSelectedNodeId] = useState(null)
+  const [showAddNodeModal, setShowAddNodeModal] = useState(false)
+
+  const selectedNode = nodes.find((node) => node.id === selectedNodeId) ?? null
+
+  function handleToggleMode() {
+    setIsPreviewMode((previous) => !previous)
+    setSelectedNodeId(null)
+  }
+
+  function handleNodeClick(node) {
+    setSelectedNodeId(node.id)
+  }
+
+  function handleCloseEditPanel() {
+    setSelectedNodeId(null)
+  }
+
+  function handleTextChange(nodeId, newText) {
+    setNodes((previousNodes) =>
+      previousNodes.map((node) =>
+        node.id === nodeId ? { ...node, text: newText } : node
+      )
+    )
+  }
+
+  function handleNodeDelete(nodeId) {
+    setNodes((previousNodes) => previousNodes.filter((node) => node.id !== nodeId))
+    if (selectedNodeId === nodeId) setSelectedNodeId(null)
+  }
+
+  function handleAddNode(nodeType, nodeText) {
+    const newNode = {
+      id: String(Date.now()),
+      type: nodeType,
+      text: nodeText,
+      position: { x: 150, y: 680 },
+      options: [],
+    }
+    setNodes((previousNodes) => [...previousNodes, newNode])
+    setShowAddNodeModal(false)
+  }
+
   return (
     <div className="app-container">
       <Navbar
         isPreviewMode={isPreviewMode}
-        onToggleMode={() => {}}
-        onAddNode={() => {}}
+        onToggleMode={handleToggleMode}
+        onAddNode={() => setShowAddNodeModal(true)}
       />
       <div className="app-body">
         {isPreviewMode ? (
-          <PreviewChat onStop={() => {}} />
+          <PreviewChat nodes={nodes} onStop={handleToggleMode} />
         ) : (
           <>
             <Canvas
-              nodes={flowData.nodes}
-              onNodeClick={() => {}}
-              onNodeDelete={() => {}}
+              nodes={nodes}
+              onNodeClick={handleNodeClick}
+              onNodeDelete={handleNodeDelete}
             />
             <EditPanel
-              node={mockSelectedNode}
-              onClose={() => {}}
-              onTextChange={() => {}}
+              node={selectedNode}
+              onClose={handleCloseEditPanel}
+              onTextChange={handleTextChange}
             />
           </>
         )}
@@ -39,8 +81,8 @@ function App() {
 
       {showAddNodeModal && (
         <AddNodeModal
-          onClose={() => {}}
-          onAddNode={() => {}}
+          onClose={() => setShowAddNodeModal(false)}
+          onAddNode={handleAddNode}
         />
       )}
     </div>
